d867b483cf357d3c9bb8ebc14cd46f1ce0398e05
Diego-LarrazM <126693992+Diego-LarrazM@users.noreply.github.com>
2024-04-10 01:32:55 +0200

    Update MatiereDao.java

--- src/main/java/INSTRUCTIF/dao/MatiereDao.java
+++ src/main/java/INSTRUCTIF/dao/MatiereDao.java
@@ -15,12 +15,22 @@ import javax.persistence.TypedQuery;
  */
 public class MatiereDao {
     public void create(Matiere matiere){
+        /**
+        * Persiste <matiere> dans la base de données selon un contexte de persistance.
+        * @param(Matiere) <matiere> : Une Matiere à persister dans la base de données.
+        */
         JpaUtil.obtenirContextePersistance().persist(matiere);
     }
     public Matiere findByLabel(String label){
+        /**
+        * Cherche dans la base de données l'entitée Matiere dont son label est <label>. Selon un contexte de persistance.
+        * @param(String) <label> : le libellé/nom de la matière cherchée.
+        * @return(Matiere) <res> : La matière idéntifiée par son label. Null si elle n'existe pas.
+        */
         TypedQuery<Matiere> query = JpaUtil.obtenirContextePersistance().createQuery("SELECT m from Matiere m WHERE m.libelle = :label", Matiere.class);
         query.setParameter("label", label);
         List<Matiere> cls = query.getResultList();
+        // S'il n'existe pas res est mis à null.
         Matiere res = null;
         if(cls.size() == 1) {
             res = cls.get(0);
@@ -29,10 +39,19 @@ public class MatiereDao {
     }
     
     public Matiere findById(Long id){
+        /**
+        * Cherche dans la base de données l'entitée Matiere à l'id <id>. Selon un contexte de persistance.
+        * @param(Long) <id> : L'id de la matière cherchée.
+        * @return(Matiere) La matière idéntifié par son id. Null s'il n'existe pas.
+        */
         return JpaUtil.obtenirContextePersistance().find(Matiere.class,id);
     }
     
     public List<Matiere> findAllAsc(){
+        /**
+        * Obtient de la base de données, avec un ordre ascendant selon leur label, toutes les entitées Matiere persistées. Selon un contexte de persistance.
+        * @return(List<Matiere>) une ArrayList avec tous les matières de la base de données. Null si aucune existe.
+        */
         return JpaUtil.obtenirContextePersistance().createQuery("SELECT m from Matiere m ORDER BY m.libelle ASC", Matiere.class).getResultList();
     }
 }
