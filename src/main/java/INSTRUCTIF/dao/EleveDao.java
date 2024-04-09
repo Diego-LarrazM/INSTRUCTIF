3a6527ac4ad480eb0d7581b50834099127516790
Diego-LarrazM <126693992+Diego-LarrazM@users.noreply.github.com>
2024-04-10 00:42:22 +0200

    Update EleveDao.java

--- src/main/java/INSTRUCTIF/dao/EleveDao.java
+++ src/main/java/INSTRUCTIF/dao/EleveDao.java
@@ -17,15 +17,24 @@ public class EleveDao {
     
     public void create(Eleve eleve){
         /**
-        * Persiste eleve dans la base de données selon un contexte de persistance.
-        * @param eleve :  Un Eleve à persister dans la table.
+        * Persiste <eleve> dans la base de données selon un contexte de persistance.
+        * @param <eleve> :  Un Eleve à persister dans la base de données.
         */
         JpaUtil.obtenirContextePersistance().persist(eleve);
     }
     public void update(Eleve eleve){
+        /**
+        * Met à jour les données associées à l'entité eleve dans la base de données selon un contexte de persistance.
+        * @param <eleve> :  Un Eleve dont ses informations sont à mettre dans la base de données.
+        */
         JpaUtil.obtenirContextePersistance().merge(eleve);
     }
     public Eleve findByMail(String email){
+        /**
+        * Cherche dans la base de données l'entité Eleve dont son email est <email>. Selon un contexte de persistance.
+        * @param <email> :  un String correspondant à un email.
+        * @return <res> :  L'élève idéntifié par son email. Null s'il n'existe pas.
+        */
         TypedQuery<Eleve> query = JpaUtil.obtenirContextePersistance().createQuery("SELECT e from Eleve e WHERE e.email = :email", Eleve.class);
         query.setParameter("email",email);
         List<Eleve> cls = query.getResultList();
